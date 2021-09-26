e40ebfaaa385e67fb908f5529da1fad49a0b2629
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-26 09:49:18 +0530

    Added: funtionality to find the salary for month

--- Empwage.java
+++ Empwage.java
@@ -2,22 +2,29 @@ package assignment1;
 
 public class Empwage {
 	public static void main(String[] args) {
-		  int wageperhour = 20;
-			 int empworkhr;
-			 
-			 double empcheck = Math.floor((Math.random()*10)%3);
-			  int i = (int)empcheck;
-			  switch(i) {
-			  case 1:
+		int workingdays = 20;
+		int wage_per_hour = 20;
+		int totalsalary = 0;
+		 int empworkhr;
+		 int salary = 0;
+		 int day =0; 
+		for(day = 1;day <= workingdays;day++) {
+			double empcheck = Math.floor((Math.random() * 10) %3);
+			int i = (int)empcheck;
+			switch(i) {
+			case 1:
 				  empworkhr = 8;
 				  break;
 			  case 2:
 				  empworkhr = 4;
 				  break;
-			  default:
-				 empworkhr = 0;
-			  }
-			 int salary = empworkhr * wageperhour;
-			 System.out.println("salary is :"+ salary);
+			 default:
+				 empworkhr = 0;	 
+			}
+			salary = empworkhr *  wage_per_hour;	
+			 System.out.println(" salary for day "+ day + " :"+ salary); 
+			 totalsalary = totalsalary + salary;
+			 System.out.println("total salary is :"+ totalsalary);	 
+		}			 
 }
 }

623cc0f995d0d2a03a4aa662192e6b0b53efd9cd
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-26 09:28:37 +0530

    Added: funtionality to find employeewage

--- Empwage.java
+++ Empwage.java
@@ -2,12 +2,20 @@ package assignment1;
 
 public class Empwage {
 	public static void main(String[] args) {
-		System.out.println("welcome to employee wage");
-		int isfulltime = 1;
-		double empcheck = Math.floor((Math.random()*10)%2);
-		if (empcheck == isfulltime)
-			System.out.println("Employee is present");
-		else 
-			System.out.println("Employee is absent");
-	}
+		 int wage_per_hour = 20;
+		  int isfulltime = 1;
+		  int emphr = 0;
+		  int empwage = 0;
+		  double empcheck = Math.floor((Math.random() * 10) %2);
+		  if(empcheck == isfulltime) {
+			  System.out.println("employee is present");
+			    emphr = 8;
+		  }else {
+			  System.out.println("employee is absent");
+			    emphr = 0;
+		  }
+		   empwage = wage_per_hour * emphr;
+		  
+		   System.out.println(" employee wage is:"+ empwage); 
+}
 }

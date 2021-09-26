8670fafd61af339b8003dffa628c9d2c6619743b
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-26 09:24:25 +0530

    Added: funtionality to check employeeattendance

--- Empwage.java
+++ Empwage.java
@@ -3,5 +3,11 @@ package assignment1;
 public class Empwage {
 	public static void main(String[] args) {
 		System.out.println("welcome to employee wage");
+		int isfulltime = 1;
+		double empcheck = Math.floor((Math.random()*10)%2);
+		if (empcheck == isfulltime)
+			System.out.println("Employee is present");
+		else 
+			System.out.println("Employee is absent");
 	}
 }
